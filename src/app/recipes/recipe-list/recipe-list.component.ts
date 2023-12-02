db312aa94fa467f4d3ae98f4303a9b0983f6f325
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-12-02 20:16:35 +0530

    config basic elements in views

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'a dummy decription',
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/09/23/0/FNK_Skillet-Chicken-Thighs_H1_s4x3.jpg.rend.hgtvcom.616.462.suffix/1632420651769.jpeg'
+    ),
+    new Recipe(
+      'A test recipe',
+      'a dummy decription',
+      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/09/23/0/FNK_Skillet-Chicken-Thighs_H1_s4x3.jpg.rend.hgtvcom.616.462.suffix/1632420651769.jpeg'
+    ),
+  ];
 }
