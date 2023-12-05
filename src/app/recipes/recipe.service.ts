956f54619b8812c2b7c7656611a5547886633358
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-12-05 15:47:45 +0530

    config routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -17,7 +17,7 @@ export class RecipeService {
     new Recipe(
       'A test recipe',
       'a dummy decription',
-      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/09/23/0/FNK_Skillet-Chicken-Thighs_H1_s4x3.jpg.rend.hgtvcom.616.462.suffix/1632420651769.jpeg',
+      'https://cdn.broadsheet.com.au/cache/10/30/1030bc60f9023eddcaf9e4686ada3ff5.jpg',
       [new Ingredient('Tomatto', 6)]
     ),
   ];
@@ -26,6 +26,10 @@ export class RecipeService {
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes.slice()[index];
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
