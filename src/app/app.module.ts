aae9ee645910b9ac27a22a6d83b4d3d806ed3754
kaveenprabodhya <kaveen.djrock40@gmail.com>
2023-12-11 19:55:17 +0530

    config dynamic alert component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,10 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { RecipeService } from './recipes/recipe.service';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterseptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -34,6 +36,8 @@ import { AuthInterseptorService } from './auth/auth-interceptor.service';
     RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective,
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,20 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthResponsedata, AuthService } from './auth.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
+import { AlertComponent } from '../shared/alert/alert.component';
+
+// const componentConfig = [
+//   {
+//     component: AlertComponent,
+//     inputs: { message: '' },
+//   },
+// ];
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
-export class AuthComponent {
+export class AuthComponent implements OnDestroy {
   isLogingMode = true;
   isLoading = false;
   error: string = null;
+  private closeSub: Subscription;
+  @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
 
   constructor(private authServie: AuthService, private router: Router) {}
+
+  ngOnDestroy(): void {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
+  }
 
   onSwitchMode() {
     this.isLogingMode = !this.isLogingMode;
@@ -46,11 +63,27 @@ export class AuthComponent {
       },
       error: (err) => {
         this.error = err;
+        this.showErrorAlert(err);
         this.isLoading = false;
       },
     });
 
     form.reset();
     this.error = null;
+  }
+
+  handleClose() {
+    this.error = null;
+  }
+
+  private showErrorAlert(message: string) {
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+    const componentRef = hostViewContainerRef.createComponent(AlertComponent);
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
   }
 }
